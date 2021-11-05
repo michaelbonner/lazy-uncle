dd777a2687c34139e26668db04e2d735144c2582
Michael Bonner <michaelwbonner@gmail.com>
2021-11-05 12:19:58 -0600

    fix lint errors in privacy page

--- pages/privacy.js
+++ pages/privacy.js
@@ -9,25 +9,25 @@ const Privacy = () => {
 
       <p className="mt-2">
         Thank you for choosing to be part of our community at Lazy
-        Uncle ("Company", "we", "us", "our"). We are committed to protecting
-        your personal information and your right to privacy. If you have any
-        questions or concerns about this privacy notice, or our practices with
-        regards to your personal information, please contact us
-        at privacy@lazyuncle.net.
+        Uncle (&ldquo;Company&rdquo;, &ldquo;we&rdquo;, &ldquo;us&rdquo;,
+        &ldquo;our&rdquo;). We are committed to protecting your personal
+        information and your right to privacy. If you have any questions or
+        concerns about this privacy notice, or our practices with regards to
+        your personal information, please contact us at privacy@lazyuncle.net.
       </p>
 
       <p className="mt-2">
-        When you visit our website https://lazyuncle.net/ (the "Website"), use
-        our mobile application, as the case may be (the "App") and more
-        generally, use any of our services (the "Services", which include
-        the Website and App), we appreciate that you are trusting us with your
-        personal information. We take your privacy very seriously. In this
-        privacy notice, we seek to explain to you in the clearest way possible
-        what information we collect, how we use it and what rights you have in
-        relation to it. We hope you take some time to read through it carefully,
-        as it is important. If there are any terms in this privacy notice that
-        you do not agree with, please discontinue use of our Services
-        immediately.
+        When you visit our website https://lazyuncle.net/ (the
+        &ldquo;Website&rdquo;), use our mobile application, as the case may be
+        (the &ldquo;App&rdquo;) and more generally, use any of our services (the
+        &ldquo;Services&rdquo;, which include the Website and App), we
+        appreciate that you are trusting us with your personal information. We
+        take your privacy very seriously. In this privacy notice, we seek to
+        explain to you in the clearest way possible what information we collect,
+        how we use it and what rights you have in relation to it. We hope you
+        take some time to read through it carefully, as it is important. If
+        there are any terms in this privacy notice that you do not agree with,
+        please discontinue use of our Services immediately.
       </p>
 
       <p className="mt-2">
@@ -102,7 +102,7 @@ const Privacy = () => {
         with us using your existing social media account details, like your
         Facebook, Twitter or other social media account. If you choose to
         register in this way, we will collect the Information described in the
-        section called "HOW DO WE HANDLE YOUR SOCIAL LOGINS" below.
+        section called &ldquo;HOW DO WE HANDLE YOUR SOCIAL LOGINS&rdquo; below.
       </p>
 
       <p className="mt-2">
@@ -147,7 +147,7 @@ const Privacy = () => {
         associated with your usage, pages and files viewed, searches and other
         actions you take such as which features you use), device event
         information (such as system activity, error reports (sometimes called
-        'crash dumps') and hardware settings).
+        &lsquo;crash dumps&rsquo;) and hardware settings).
       </p>
 
       <p className="mt-2">Information collected through our App</p>
@@ -165,7 +165,7 @@ const Privacy = () => {
         ▪ Push Notifications. We may request to send you push notifications
         regarding your account or certain features of the App. If you wish to
         opt-out from receiving these types of communications, you may turn them
-        off in your device's settings.
+        off in your device&apos;s settings.
       </p>
       <p className="mt-2">
         This information is primarily needed to maintain the security and
@@ -200,7 +200,8 @@ const Privacy = () => {
         or Facebook account), we use the information you allowed us to collect
         from those third parties to facilitate account creation and logon
         process for the performance of the contract. See the section below
-        headed "HOW DO WE HANDLE YOUR SOCIAL LOGINS" for further information.
+        headed &ldquo;HOW DO WE HANDLE YOUR SOCIAL LOGINS&rdquo; for further
+        information.
       </p>
 
       <p className="mt-2 ml-4">
@@ -219,7 +220,8 @@ const Privacy = () => {
 
       <p className="mt-2 ml-4">
         ▪ To enable user-to-user communications. We may use your information in
-        order to enable user-to-user communications with each user's consent.
+        order to enable user-to-user communications with each user&apos;s
+        consent.
       </p>
 
       <p className="mt-2 ml-4">
@@ -477,15 +479,16 @@ const Privacy = () => {
 
       <p className="mt-2">
         Most web browsers and some mobile operating systems and mobile
-        applications include a Do-Not-Track ("DNT") feature or setting you can
-        activate to signal your privacy preference not to have data about your
-        online browsing activities monitored and collected. At this stage no
-        uniform technology standard for recognizing and implementing DNT signals
-        has been finalized. As such, we do not currently respond to DNT browser
-        signals or any other mechanism that automatically communicates your
-        choice not to be tracked online. If a standard for online tracking is
-        adopted that we must follow in the future, we will inform you about that
-        practice in a revised version of this privacy notice. 
+        applications include a Do-Not-Track (&ldquo;DNT&rdquo;) feature or
+        setting you can activate to signal your privacy preference not to have
+        data about your online browsing activities monitored and collected. At
+        this stage no uniform technology standard for recognizing and
+        implementing DNT signals has been finalized. As such, we do not
+        currently respond to DNT browser signals or any other mechanism that
+        automatically communicates your choice not to be tracked online. If a
+        standard for online tracking is adopted that we must follow in the
+        future, we will inform you about that practice in a revised version of
+        this privacy notice. 
       </p>
 
       <h2 className="text-lg font-semibold mt-4">
@@ -498,10 +501,10 @@ const Privacy = () => {
       </p>
 
       <p className="mt-2">
-        California Civil Code Section 1798.83, also known as the "Shine The
-        Light" law, permits our users who are California residents to request
-        and obtain from us, once a year and free of charge, information about
-        categories of personal information (if any) we disclosed to third
+        California Civil Code Section 1798.83, also known as the &ldquo;Shine
+        The Light&rdquo; law, permits our users who are California residents to
+        request and obtain from us, once a year and free of charge, information
+        about categories of personal information (if any) we disclosed to third
         parties for direct marketing purposes and the names and addresses of all
         third parties with which we shared personal information in the
         immediately preceding calendar year. If you are a California resident
@@ -532,12 +535,12 @@ const Privacy = () => {
 
       <p className="mt-2">
         We may update this privacy notice from time to time. The updated version
-        will be indicated by an updated "Revised" date and the updated version
-        will be effective as soon as it is accessible. If we make material
-        changes to this privacy notice, we may notify you either by prominently
-        posting a notice of such changes or by directly sending you a
-        notification. We encourage you to review this privacy notice frequently
-        to be informed of how we are protecting your information.
+        will be indicated by an updated &ldquo;Revised&rdquo; date and the
+        updated version will be effective as soon as it is accessible. If we
+        make material changes to this privacy notice, we may notify you either
+        by prominently posting a notice of such changes or by directly sending
+        you a notification. We encourage you to review this privacy notice
+        frequently to be informed of how we are protecting your information.
       </p>
 
       <h2 className="text-lg font-semibold mt-4">
