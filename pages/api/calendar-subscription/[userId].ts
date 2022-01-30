e54b589a032602e02766889d00f378c26803e65c
Michael Bonner <michaelwbonner@gmail.com>
2022-01-30 12:24:43 -0700

    show birthdays back a couple years

--- pages/api/calendar-subscription/[userId].ts
+++ pages/api/calendar-subscription/[userId].ts
@@ -22,7 +22,7 @@ export default async function handler(
 
   const events = [] as Birthdate[];
   birthdays.forEach((birthday) => {
-    for (let index = 0; index < 20; index++) {
+    for (let index = -2; index < 12; index++) {
       const userBirthday = parse(birthday.date, "yyyy-MM-dd", new Date());
       const birthDate = setYear(
         userBirthday,
