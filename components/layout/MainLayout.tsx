0f376f33a1534285b11fc1108a295c00fb79478e
Michael Bonner <michaelwbonner@gmail.com>
2022-02-01 14:04:14 -0700

    default text color is gray-50

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -14,7 +14,7 @@ const MainLayout = ({
   const { data: session } = useSession();
 
   return (
-    <div className="bg-gray-700 text-indigo-50 min-h-screen">
+    <div className="bg-gray-700 text-gray-50 min-h-screen">
       <Head>
         <title>{title}</title>
         <meta
