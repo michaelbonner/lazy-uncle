c59c256a4270abcc1439c3099b27672caeb1d431
Michael Bonner <michaelwbonner@gmail.com>
2022-02-06 14:04:07 -0700

    try pwags

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  pwa: {
+    dest: "public",
+  },
+};
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig);
