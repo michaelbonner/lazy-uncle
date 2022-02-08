400e4a9ba43a7e49a67798b47b6b9641deea7a4d
Michael Bonner <michaelwbonner@gmail.com>
2022-02-07 18:01:57 -0700

    add prisma disconnect on scripts

--- scripts/addBirthdaysForUser.ts
+++ scripts/addBirthdaysForUser.ts
@@ -83,7 +83,11 @@ async function main() {
   );
 }
 
-main().catch((e) => {
-  console.error(e);
-  process.exit(1);
-});
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- scripts/showUsersAndCountOfBirthdaysForUser.ts
+++ scripts/showUsersAndCountOfBirthdaysForUser.ts
@@ -32,7 +32,11 @@ async function main() {
   );
 }
 
-main().catch((e) => {
-  console.error(e);
-  process.exit(1);
-});
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
