3978ab0f0a8cb00046b403308027540ecaf0c059
Michael Bonner <michaelwbonner@gmail.com>
2022-02-09 20:15:06 -0700

    add gtm

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
-import client from "../lib/apollo";
 import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 import { ToastContainer } from "react-toastify";
-
 import "react-toastify/dist/ReactToastify.css";
+import client from "../lib/apollo";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: "GTM-W76BXL4" });
+  }, []);
+
   return (
     <SessionProvider session={pageProps.session}>
       <ApolloProvider client={client}>
