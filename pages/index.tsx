b4fb276389bcc4e445e9db2ba923966c324f84a3
Michael Bonner <michaelwbonner@gmail.com>
2022-02-12 17:00:03 -0700

    fix login redirect not showing app first time

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps } from "next";
 import { Session } from "next-auth";
 import { Provider } from "next-auth/providers";
-import { getProviders, getSession } from "next-auth/react";
+import { getProviders, getSession, useSession } from "next-auth/react";
 import React from "react";
 import BirthdaysContainer from "../components/BirthdaysContainer";
 import MainLayout from "../components/layout/MainLayout";
@@ -14,10 +14,12 @@ function Home({
   providers: Provider[];
   session: Session;
 }) {
+  const { status: sessionStatus } = useSession();
+
   return (
     <MainLayout title="Lazy Uncle">
       <main className="max-w-7xl mx-auto pb-8 px-2">
-        {session?.user ? (
+        {session?.user || sessionStatus === "authenticated" ? (
           <BirthdaysContainer userId={session?.user?.id} />
         ) : (
           <Welcome providers={providers} />
