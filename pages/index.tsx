6de30087f4c910f6d53e63ece520ebb96edfa07c
Michael Bonner <michaelwbonner@gmail.com>
2022-02-14 12:22:07 -0700

    loading animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,24 @@ function Home({ session }: { providers: Provider[]; session: Session }) {
   return (
     <MainLayout title="Lazy Uncle">
       <main className="max-w-7xl mx-auto pb-8 px-2">
+        {sessionStatus === "loading" && (
+          <div className="flex items-center justify-center">
+            <div className="circle w-16 h-16 mx-auto">
+              <svg
+                className="circle__svg"
+                viewBox="0 0 100 100"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle
+                  className="circle__svg-circle fill-transparent stroke-current text-teal-50"
+                  cx="50"
+                  cy="50"
+                  r="45"
+                />
+              </svg>
+            </div>
+          </div>
+        )}
         {sessionStatus === "authenticated" && (
           <BirthdaysContainer userId={session?.user?.id} />
         )}
