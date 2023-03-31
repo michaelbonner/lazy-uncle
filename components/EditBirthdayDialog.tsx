9ea4a642d992bc884f259e8aba8d007e8f0c20be
Michael Bonner <michaelwbonner@gmail.com>
2023-03-31 14:34:28 -0600

    --ammend

---
+++ components/EditBirthdayDialog.tsx
@@ -0,0 +1,63 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import { NexusGenObjects } from "../generated/nexus-typegen";
+import EditBirthdayForm from "./EditBirthdayForm";
+
+const EditBirthdayDialog = ({
+  birthday,
+  isOpen,
+  handleClose,
+}: {
+  birthday: NexusGenObjects["Birthday"];
+  isOpen: boolean;
+  handleClose: () => void;
+}) => {
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={handleClose}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center text-gray-800">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Edit {birthday.name}
+                  </Dialog.Title>
+                  <EditBirthdayForm
+                    birthday={birthday}
+                    handleClose={handleClose}
+                  />
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
+
+export default EditBirthdayDialog;
