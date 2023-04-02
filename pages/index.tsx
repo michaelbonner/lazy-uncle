37ea49c6774061daaa18036767507e7000b3a8e4
Michael Bonner <michaelwbonner@gmail.com>
2023-04-02 14:39:34 -0600

    use server session for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import { useSession } from "next-auth/react";
+import { GetServerSideProps, NextPage } from "next";
+import { Session, getServerSession } from "next-auth";
 import dynamic from "next/dynamic";
-import LoadingSpinner from "../components/LoadingSpinner";
 import Welcome from "../components/Welcome";
 import MainLayout from "../components/layout/MainLayout";
+import { authOptions } from "./api/auth/[...nextauth]";
 
 const BirthdaysContainer = dynamic(
   () => import("../components/BirthdaysContainer"),
@@ -15,24 +16,32 @@ const BirthdaysContainer = dynamic(
   }
 );
 
-function Home() {
-  const { data: session, status: sessionStatus } = useSession();
+type HomeProps = {
+  session: Session;
+};
 
+export const Home: NextPage<HomeProps> = ({ session }) => {
   return (
     <MainLayout title="Lazy Uncle">
       <main className="mx-auto max-w-7xl px-2 pb-8">
-        {sessionStatus === "loading" && (
-          <div className="flex items-center justify-center">
-            <LoadingSpinner />
-          </div>
-        )}
-        {sessionStatus === "authenticated" && (
-          <BirthdaysContainer userId={session?.user?.id} />
-        )}
-        {sessionStatus === "unauthenticated" && <Welcome />}
+        {session?.user?.id && <BirthdaysContainer userId={session.user.id} />}
+        {!session?.user?.id && <Welcome />}
       </main>
     </MainLayout>
   );
-}
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  delete session?.user?.emailVerified;
+  delete session?.user?.createdAt;
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
 
 export default Home;
