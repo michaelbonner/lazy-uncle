171e4ec47b121fc0c5b20c4129dd98316f36e73e
Michael Bonner <michaelwbonner@gmail.com>
2023-07-17 10:13:32 -0600

    use graphql-yoga instead of apollo server

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,48 +1,22 @@
-import { ApolloServer } from "apollo-server-micro";
-import { IncomingMessage, ServerResponse } from "http";
-import cors from "micro-cors";
-import { RequestHandler } from "next/dist/server/next";
+import { useApolloServerErrors } from "@envelop/apollo-server-errors";
+import { createYoga } from "graphql-yoga";
 import { createContext } from "../../graphql/context";
 import { schema } from "../../graphql/schema";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default createYoga<{
+  req: NextApiRequest;
+  res: NextApiResponse;
+}>({
+  schema,
+  context: createContext,
+  graphqlEndpoint: "/api/graphql",
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  plugins: [useApolloServerErrors()],
+});
 
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-
-const apolloServer = new ApolloServer({
-  cache: "bounded",
-  context: createContext,
-  schema,
-});
-
-let apolloServerHandler: RequestHandler;
-
-async function getApolloServerHandler() {
-  if (!apolloServerHandler) {
-    await apolloServer.start();
-
-    apolloServerHandler = apolloServer.createHandler({
-      path: "/api/graphql",
-    });
-  }
-
-  return apolloServerHandler;
-}
-
-const handler: RequestHandler = async (
-  req: IncomingMessage,
-  res: ServerResponse
-) => {
-  const apolloServerHandler = await getApolloServerHandler();
-
-  if (req.method === "OPTIONS") {
-    res.end();
-    return;
-  }
-
-  return apolloServerHandler(req, res);
-};
-
-export default cors()(handler);
