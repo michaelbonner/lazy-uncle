832ba83f3bb88a486bda22f26b58d1a0841a91fd
Michael Bonner <michaelwbonner@gmail.com>
2023-07-25 14:35:14 -0600

    fix 365 days from now on birthdays upcoming

--- shared/getDaysUntilNextBirthday.ts
+++ shared/getDaysUntilNextBirthday.ts
@@ -17,24 +17,6 @@ export const getDaysUntilNextBirthday = (
   const birthDateMonthAndDay = format(birthDate, "MM-dd");
   const thisYearBirthday = parse(birthDateMonthAndDay, "MM-dd", new Date());
 
-  if(birthday.id === 'clkiijv1j0001l808vybsa6me') {
-    console.log('birthday', birthday)
-    console.log('thisYearBirthday', thisYearBirthday)
-    console.log('isFuture(thisYearBirthday)', isFuture(thisYearBirthday))
-    console.log('isToday(thisYearBirthday)', isToday(thisYearBirthday))
-    console.log('addYears(thisYearBirthday, 1);', addYears(thisYearBirthday, 1))
-
-    const nextBirthday =
-    isFuture(thisYearBirthday) || isToday(thisYearBirthday)
-      ? thisYearBirthday
-      : addYears(thisYearBirthday, 1);
-      
-    console.log('nextBirthday', nextBirthday)
-    const daysFromNow = differenceInDays(nextBirthday, startOfDay(new Date()));
-    console.log('daysFromNow', daysFromNow)
-
-  }
-
   const nextBirthday =
     isFuture(thisYearBirthday) || isToday(thisYearBirthday)
       ? thisYearBirthday
