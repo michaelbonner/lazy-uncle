19488e7e9374049f7bd231f3eccd849044b07ad8
Michael Bonner <michaelwbonner@gmail.com>
2024-10-26 17:17:08 -0600

    npm updates

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,16 +1,38 @@
 import { useApolloServerErrors } from "@envelop/apollo-server-errors";
 import { createYoga } from "graphql-yoga";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { createContext } from "../../graphql/context";
 import { schema } from "../../graphql/schema";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { aj } from "../../lib/arcjet";
 
-export default createYoga<{
-  req: NextApiRequest;
-  res: NextApiResponse;
-}>({
-  schema,
-  context: createContext,
-  graphqlEndpoint: "/api/graphql",
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  plugins: [useApolloServerErrors()],
-});
+const defaultExport = async (req: NextApiRequest, res: NextApiResponse) => {
+  const decision = await aj.protect(req, { requested: 1 });
+
+  if (decision.isDenied()) {
+    if (decision.reason.isRateLimit()) {
+      return res
+        .status(429)
+        .json({ error: "Too Many Requests", reason: decision.reason });
+    } else if (decision.reason.isBot()) {
+      return res
+        .status(403)
+        .json({ error: "No bots allowed", reason: decision.reason });
+    } else {
+      return res
+        .status(403)
+        .json({ error: "Forbidden", reason: decision.reason });
+    }
+  }
+
+  return createYoga<{
+    req: NextApiRequest;
+    res: NextApiResponse;
+  }>({
+    schema,
+    context: createContext,
+    graphqlEndpoint: "/api/graphql",
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    plugins: [useApolloServerErrors()],
+  });
+};
+export default defaultExport;
