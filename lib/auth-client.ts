7d798f694453ee80def0d8d60b54932bd7a3bae5
Michael Bonner <mike@bootpackdigital.com>
2025-08-24 22:33:28 -0600

    Update baseURL for auth client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,8 @@
 import { createAuthClient } from "better-auth/react";
 
-export const authClient = createAuthClient();
+export const authClient = createAuthClient({
+  /** The base URL of the server (optional if you're using the same domain) */
+  baseURL: process.env.VERCEL_URL ?? "http://localhost:3000",
+});
 
 export type Session = typeof authClient.$Infer.Session;
