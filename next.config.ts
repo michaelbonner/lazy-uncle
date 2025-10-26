ba8afdc0711fcfa480c4ea1fd6946f1c5906f704
Michael Bonner <mike@bootpackdigital.com>
2025-10-26 15:02:11 -0600

    run build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    reactCompiler: true,
-  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
